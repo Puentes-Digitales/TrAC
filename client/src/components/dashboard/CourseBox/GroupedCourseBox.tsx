49d477bd461f915cba2e2d7a6426a1412b72fe1b
Fabián Morag <fabianmorag0405@gmail.com>
2021-01-29 19:20:44 -0300

    new grouped components

--- client/src/components/dashboard/CourseBox/GroupedCourseBox.tsx
+++ client/src/components/dashboard/CourseBox/GroupedCourseBox.tsx
@@ -1,10 +1,8 @@
 import { AnimatePresence, motion } from "framer-motion";
 import { some, truncate } from "lodash";
-import dynamic from "next/dynamic";
 import React, { FC, memo, useContext, useMemo } from "react";
 
 import {
-  Badge,
   Box,
   Flex,
   Stack,
@@ -13,190 +11,116 @@ import {
   useColorModeValue,
 } from "@chakra-ui/react";
 
-import { StateCourse } from "../../../../constants";
 import { ConfigContext } from "../../../context/Config";
 import {
   CoursesDashboardStore,
-  pairTermYear,
   toggleOpenCourse,
 } from "../../../context/CoursesDashboard";
-import {
-  ForeplanActiveStore,
-  ForeplanHelperStore,
-} from "../../../context/ForeplanContext";
 import { track } from "../../../context/Tracking";
-import {
-  TEXT_WHITE_SHADOW,
-  width100percent,
-} from "../../../utils/cssConstants";
-import { useUser } from "../../../utils/useUser";
-import { PredictState } from "../../foreplan/courseBox/PredictState";
-import {
-  HistogramHistoric,
-  HistogramNow,
-  HistogramsComponent,
-} from "../Histogram";
-import { HistoricalCirclesComponent } from "../HistoricalCirclesComponent";
+import { HistogramHistoric, HistogramsComponent } from "../Histogram";
 
 import type {
   ICourse,
   IGroupedCourse,
   ITakenCourse,
-  ITakenSemester,
 } from "../../../../../interfaces";
-const ForeplanCourseCheckbox = dynamic(
-  () => import("../../foreplan/courseBox/Checkbox")
-);
-
-const ForeplanCourseStats = dynamic(
-  () => import("../../foreplan/courseBox/Stats")
-);
 
 export type CurrentTakenData = Partial<ITakenCourse>;
-
-const useIsCourseFuturePlanificationFulfilled = ({
-  state,
-  code,
-}: {
-  state?: StateCourse;
-  code: string;
-}) => {
-  const { user } = useUser({
-    fetchPolicy: "cache-only",
-  });
-  const isForeplanActive = ForeplanActiveStore.hooks.useIsForeplanActive();
-  const isPossibleToTakeForeplan = ForeplanActiveStore.hooks.useIsPossibleToTakeForeplan(
-    { state, course: code },
-    [state, code]
-  );
-  const isDirectTake = ForeplanHelperStore.hooks.useForeplanIsDirectTake(code);
-  const isPredictedDirectTake = ForeplanActiveStore.hooks.useIsDirectTakePredicted(
-    code
-  );
-  const isFutureCourseRequisitesFulfilled = ForeplanActiveStore.hooks.useForeplanIsFutureCourseRequisitesFulfilled(
-    code
-  );
-
-  if (!user?.config.FOREPLAN_FUTURE_COURSE_PLANIFICATION) {
-    return false;
-  }
-
-  if (!isForeplanActive) {
-    return false;
-  }
-
-  if (isPossibleToTakeForeplan) {
-    if (isDirectTake || isPredictedDirectTake) {
-      return true;
-    }
-    if (isFutureCourseRequisitesFulfilled) {
-      return true;
-    }
-  }
-
-  return false;
-};
 
 const OuterCourseBox: FC<
   Pick<ICourse, "code" | "historicDistribution"> & {
     isOpen: boolean;
     borderColor: string;
-    semestersTaken: ITakenSemester[];
-  } & Pick<CurrentTakenData, "currentDistribution"> & {
-      isFutureCourseFulfilled: boolean;
+  }
+> = memo(({ children, code, historicDistribution, borderColor, isOpen }) => {
+  const config = useContext(ConfigContext);
+
+  const activeCourse = CoursesDashboardStore.hooks.useActiveCourse(code);
+
+  const { height, width } = useMemo(() => {
+    let height: number | undefined = undefined;
+    let width: number | undefined = undefined;
+    if (isOpen) {
+      if (
+        historicDistribution &&
+        some(historicDistribution, ({ value }) => value)
+      ) {
+        width = 350;
+        height = 350 - 130;
+      }
+    } else {
+      width = 180;
+      height = 120;
     }
-> = memo(
-  ({
-    children,
-    code,
-    currentDistribution,
-    historicDistribution,
-    semestersTaken,
-    borderColor,
-    isOpen,
-    isFutureCourseFulfilled,
-  }) => {
+    if (!width) {
+      width = 220;
+    }
+    if (!height) {
+      height = 140;
+    }
+
+    return { height, width };
+  }, [isOpen, historicDistribution]);
+
+  const borderWidth = activeCourse
+    ? config.COURSE_BOX_BORDER_WIDTH_ACTIVE
+    : config.COURSE_BOX_BORDER_WIDTH_INACTIVE;
+
+  const boxShadow = `0px 0px 0px ${borderWidth} ${borderColor}`;
+
+  const color = useColorModeValue(config.COURSE_BOX_TEXT_COLOR, "white");
+  return (
+    <Flex
+      m={1}
+      color={color}
+      width={width}
+      height={height}
+      borderRadius={5}
+      boxShadow={boxShadow}
+      transition={config.COURSE_BOX_ALL_TRANSITION_DURATION}
+      className="unselectable courseBox"
+      padding={0}
+      overflow="hidden"
+    >
+      {children}
+    </Flex>
+  );
+});
+
+const MainBlockOuter: FC<Pick<ICourse, "code" | "flow" | "requisites">> = memo(
+  ({ children, code, flow, requisites }) => {
     const config = useContext(ConfigContext);
-
-    const activeCourse = CoursesDashboardStore.hooks.useActiveCourse(code);
-    const explicitSemester = CoursesDashboardStore.hooks.useExplicitSemester();
-
-    const isExplicitSemester = CoursesDashboardStore.hooks.useCheckExplicitSemester(
-      semestersTaken
-    );
-
-    const opacity = useMemo(() => {
-      if (activeCourse) {
-        return 1;
-      }
-      if (explicitSemester) {
-        if (isExplicitSemester) {
-          return 1;
-        }
-        return 0.5;
-      }
-      return 1;
-    }, [code, activeCourse, explicitSemester, isExplicitSemester]);
-
-    const { height, width } = useMemo(() => {
-      let height: number | undefined = undefined;
-      let width: number | undefined = undefined;
-      if (isOpen) {
-        if (
-          currentDistribution &&
-          some(currentDistribution, ({ value }) => value)
-        ) {
-          width = 350;
-          if (
-            historicDistribution &&
-            some(historicDistribution, ({ value }) => value)
-          ) {
-            height = 350;
-          } else {
-            height = 350 - 130;
-          }
-        } else if (
-          historicDistribution &&
-          some(historicDistribution, ({ value }) => value)
-        ) {
-          width = 350;
-          height = 350 - 130;
-        }
-      } else {
-        width = 180;
-        height = 120;
-      }
-      if (!width) {
-        width = 220;
-      }
-      if (!height) {
-        height = 140;
-      }
-
-      return { height, width };
-    }, [isOpen, historicDistribution, currentDistribution]);
-
-    const borderWidth =
-      activeCourse || isFutureCourseFulfilled
-        ? config.COURSE_BOX_BORDER_WIDTH_ACTIVE
-        : config.COURSE_BOX_BORDER_WIDTH_INACTIVE;
-
-    const boxShadow = `0px 0px 0px ${borderWidth} ${borderColor}`;
-
-    const color = useColorModeValue(config.COURSE_BOX_TEXT_COLOR, "white");
+    const bg = useColorModeValue(config.COURSE_BOX_BACKGROUND_COLOR, "#1A202C");
     return (
       <Flex
-        m={1}
-        color={color}
-        width={width}
-        height={height}
-        borderRadius={5}
-        opacity={opacity}
-        boxShadow={boxShadow}
-        transition={config.COURSE_BOX_ALL_TRANSITION_DURATION}
-        className="unselectable courseBox"
-        padding={0}
-        overflow="hidden"
+        w="100%"
+        h="100%"
+        pt={2}
+        pl={2}
+        pos="relative"
+        className="mainBlock"
+        cursor="pointer"
+        borderRadius="5px 0px 0x 5px"
+        bg={bg}
+        onClick={() => {
+          toggleOpenCourse(code, (wasOpen) => {
+            if (!wasOpen) {
+              CoursesDashboardStore.actions.addCourse({
+                course: code,
+                flow,
+                requisites,
+              });
+            } else {
+              CoursesDashboardStore.actions.removeCourse(code);
+            }
+
+            track({
+              action: "click",
+              target: `course-box-${code}`,
+              effect: `${wasOpen ? "close" : "open"}-course-box`,
+            });
+          });
+        }}
       >
         {children}
       </Flex>
@@ -204,72 +128,15 @@ const OuterCourseBox: FC<
   }
 );
 
-const MainBlockOuter: FC<
-  Pick<ICourse, "code" | "flow" | "requisites"> & {
-    semestersTaken: ITakenSemester[];
-  }
-> = memo(({ children, code, flow, requisites, semestersTaken }) => {
-  const config = useContext(ConfigContext);
-  const bg = useColorModeValue(config.COURSE_BOX_BACKGROUND_COLOR, "#1A202C");
-  return (
-    <Flex
-      w="100%"
-      h="100%"
-      pt={2}
-      pl={2}
-      pos="relative"
-      className="mainBlock"
-      cursor="pointer"
-      borderRadius="5px 0px 0x 5px"
-      bg={bg}
-      onClick={() => {
-        toggleOpenCourse(code, (wasOpen) => {
-          if (!wasOpen) {
-            CoursesDashboardStore.actions.addCourse({
-              course: code,
-              flow,
-              requisites,
-              semestersTaken,
-            });
-          } else {
-            CoursesDashboardStore.actions.removeCourse(code);
-          }
-
-          track({
-            action: "click",
-            target: `course-box-${code}`,
-            effect: `${wasOpen ? "close" : "open"}-course-box`,
-          });
-        });
-      }}
-    >
-      {children}
-    </Flex>
-  );
-});
-
 const NameComponent: FC<
-  Pick<ICourse, "code" | "taken" | "name"> & {
+  Pick<ICourse, "code" | "name"> & {
     isOpen: boolean;
-  } & Pick<CurrentTakenData, "parallelGroup">
-> = memo(({ code, name, taken, parallelGroup, isOpen }) => {
-  const config = useContext(ConfigContext);
+  }
+> = memo(({ code, name, isOpen }) => {
+  // const config = useContext(ConfigContext);
   return (
     <Stack spacing={1}>
-      <Flex alignItems="center">
-        <Text m={0} whiteSpace="nowrap">
-          <b>{taken?.find(({ equiv }) => equiv)?.equiv || code}</b>
-        </Text>
-        {!!parallelGroup && (
-          <Badge
-            ml={2}
-            backgroundColor={config.PARALLEL_GROUP_BACKGROUND_COLOR}
-            color={config.PARALLEL_GROUP_LABEL_COLOR}
-          >
-            {config.PARALLEL_GROUP_LABEL}: {parallelGroup}
-          </Badge>
-        )}
-      </Flex>
+      <Flex alignItems="center"></Flex>
 
       <Text fontSize={9} maxWidth="150px" pr={1}>
         {truncate(name, { length: isOpen ? 60 : 35 })}
@@ -279,12 +146,11 @@ const NameComponent: FC<
 });
 
 const SecondaryBlockOuter: FC<
-  Pick<ICourse, "bandColors"> &
-    Pick<CurrentTakenData, "grade" | "state"> & {
-      n_total: number;
-      n_passed: number;
-      borderColor: string;
-    }
+  Pick<ICourse, "bandColors"> & {
+    n_total: number;
+    n_passed: number;
+    borderColor: string;
+  }
 > = memo(({ children, borderColor, n_total, n_passed }) => {
   const config = useContext(ConfigContext);
 
@@ -292,11 +158,11 @@ const SecondaryBlockOuter: FC<
 
   const stateColor = useMemo(() => {
     if (n_passed == 0) {
-      return "rgb(255,255,255)";
+      return "#f2f0f7";
     } else if (n_passed < n_total * 0.25) {
-      return "rgb(210,180,222)";
+      return "#cbc9e2";
     } else if (n_passed < n_total * 0.5) {
-      return "rgb(165,105,189)";
+      return "#";
     } else if (n_passed < n_total * 0.75) {
       return "rgb(125,60,152)";
     } else {
@@ -322,30 +188,6 @@ const SecondaryBlockOuter: FC<
     </Flex>
   );
 });
-
-const RegistrationComponent: FC<Pick<CurrentTakenData, "registration">> = memo(
-  ({ registration }) => {
-    return (
-      <motion.div
-        key="status"
-        initial={{
-          opacity: 0,
-          position: "static",
-        }}
-        animate={{ opacity: 1 }}
-        exit={{
-          opacity: 0,
-          position: "absolute",
-        }}
-        css={width100percent}
-      >
-        <Text fontSize="9px" mr={2} textAlign="end">
-          <b>{registration}</b>
-        </Text>
-      </motion.div>
-    );
-  }
-);
 
 const CreditsComponent: FC<Pick<ICourse, "credits">> = memo(({ credits }) => {
   return (
@@ -428,42 +270,6 @@ export const ReqCircleComponent: FC<Pick<ICourse, "code">> = memo(
   }
 );
 
-const GradeComponent: FC<Pick<CurrentTakenData, "state" | "grade">> = memo(
-  ({ grade, state }) => {
-    const config = useContext(ConfigContext);
-
-    return (
-      <Text
-        mb={2}
-        pt={1}
-        textShadow={TEXT_WHITE_SHADOW}
-        color="black"
-        fontWeight="bold"
-      >
-        {(() => {
-          if (grade && grade != -1) {
-            return grade.toFixed(1);
-          }
-          switch (state) {
-            case StateCourse.Passed:
-              return config.STATE_PASSED_LABEL_MINI;
-            case StateCourse.Failed:
-              return config.STATE_FAILED_LABEL_MINI;
-            case StateCourse.Canceled:
-              return config.STATE_CANCELED_LABEL_MINI;
-            case StateCourse.Pending:
-              return config.STATE_PENDING_LABEL_MINI;
-            case StateCourse.Current:
-              return config.STATE_CURRENT_LABEL_MINI;
-            default:
-              return "BUGasd";
-          }
-        })()}
-      </Text>
-    );
-  }
-);
-
 export const currentDistributionLabel = ({
   term,
   year,
@@ -480,77 +286,24 @@ export function GroupedCourseBox({
   code,
   name,
   credits,
-  groupedDistribution,
   historicDistribution,
-  n_total,
-  n_passed,
-  taken,
   bandColors,
   requisites,
   flow,
 }: IGroupedCourse) {
   const config = useContext(ConfigContext);
 
-  const semestersTaken = useMemo(() => {
-    const semestersTaken = taken.map(({ term, year }) => {
-      return { term, year };
-    });
-
-    return semestersTaken;
-  }, [taken]);
-
   const activeCourse = CoursesDashboardStore.hooks.useActiveCourse(code);
   const activeFlow = CoursesDashboardStore.hooks.useActiveFlow(code);
   const activeRequisites = CoursesDashboardStore.hooks.useActiveRequisites(
     code
   );
-  const explicitSemester = CoursesDashboardStore.hooks.useCheckExplicitSemester(
-    semestersTaken
-  );
-
-  const { user } = useUser({
-    fetchPolicy: "cache-only",
-  });
-
-  const {
-    state,
-    grade,
-    registration,
-    currentDistribution,
-    term,
-    year,
-    parallelGroup,
-  } = useMemo<Partial<ITakenCourse>>(() => {
-    if (explicitSemester) {
-      const foundData = taken.find(({ term, year }) => {
-        return pairTermYear(term, year) === explicitSemester;
-      });
-      return foundData || {};
-    }
-    return taken[0] || {};
-  }, [semestersTaken, explicitSemester, taken]);
 
   const isOpen = CoursesDashboardStore.hooks.useDashboardIsCourseOpen(code);
-
-  const isFutureCourseFulfilled = useIsCourseFuturePlanificationFulfilled({
-    state: taken[0]?.state,
-    code,
-  });
-
-  const isForeplanActive = ForeplanActiveStore.hooks.useIsForeplanActive();
-
-  const isPossibleToTakeForeplan = ForeplanActiveStore.hooks.useIsPossibleToTakeForeplan(
-    { state: taken[0]?.state, course: code },
-    [taken, code]
-  );
 
   const borderColor = (() => {
     if (activeCourse) {
       return config.ACTIVE_COURSE_BOX_COLOR;
-    }
-
-    if (isFutureCourseFulfilled) {
-      return config.FOREPLAN_COURSE_FUTURE_PLANIFICATION_FULFILLED_BORDER_COLOR;
     }
 
     if (activeFlow) {
@@ -559,113 +312,40 @@ export function GroupedCourseBox({
     if (activeRequisites) {
       return config.REQUISITE_COURSE_BOX_COLOR;
     }
-    if (explicitSemester) {
-      return config.EXPLICIT_SEMESTER_COURSE_BOX_COLOR;
-    }
     return config.INACTIVE_COURSE_BOX_COLOR;
   })();
-
-  const shouldPredictCourseState = useMemo(() => {
-    if (
-      !isForeplanActive ||
-      !user?.config.FOREPLAN_PREDICT_CURRENT_COURSE ||
-      (taken[0]?.state !== StateCourse.Current &&
-        taken[0]?.state !== StateCourse.Pending)
-    ) {
-      return false;
-    }
-
-    return true;
-  }, [isForeplanActive, code, requisites, taken, user]);
 
   return (
     <OuterCourseBox
       code={code}
-      currentDistribution={currentDistribution}
       historicDistribution={historicDistribution}
       isOpen={isOpen}
-      semestersTaken={semestersTaken}
       borderColor={borderColor}
-      isFutureCourseFulfilled={isFutureCourseFulfilled}
     >
-      <MainBlockOuter
-        flow={flow}
-        requisites={requisites}
-        code={code}
-        semestersTaken={semestersTaken}
-      >
-        <NameComponent
-          code={code}
-          taken={taken}
-          name={name}
-          isOpen={isOpen}
-          parallelGroup={parallelGroup}
-        />
+      <MainBlockOuter flow={flow} requisites={requisites} code={code}>
+        <NameComponent code={code} name={name} isOpen={isOpen} />
 
         <AnimatePresence>
-          {registration && isOpen && (
-            <RegistrationComponent
-              key="registration"
-              registration={registration}
-            />
-          )}
           {!isOpen && <CreditsComponent key="credits" credits={credits} />}
 
           <ReqCircleComponent key="reqCircle" code={code} />
 
           {isOpen && (
-            <HistogramsComponent
-              key="histogramsComponent"
-              code={code}
-              state={taken[0]?.state}
-            >
-              <HistogramNow
-                taken={taken}
-                bandColors={bandColors}
-                currentDistribution={currentDistribution}
-                term={term}
-                year={year}
-                grade={grade}
-              />
+            <HistogramsComponent key="histogramsComponent" code={code}>
               <HistogramHistoric
                 historicDistribution={historicDistribution}
                 bandColors={bandColors}
-                grade={grade}
               />
             </HistogramsComponent>
-          )}
-          {isPossibleToTakeForeplan && user?.config.FOREPLAN_COURSE_STATS && (
-            <ForeplanCourseStats key="foreplanCourseStats" code={code} />
           )}
         </AnimatePresence>
       </MainBlockOuter>
       <SecondaryBlockOuter
         bandColors={bandColors}
-        grade={grade}
-        state={state}
-        n_total={n_total}
-        n_passed={n_passed}
+        n_passed={56}
+        n_total={100}
         borderColor={borderColor}
-      >
-        {grade !== undefined && <GradeComponent grade={grade} state={state} />}
-
-        {taken.length > 1 && (
-          <HistoricalCirclesComponent taken={taken} isOpen={isOpen} />
-        )}
-        {shouldPredictCourseState && (
-          <PredictState code={code} isCourseOpen={isOpen} />
-        )}
-        <AnimatePresence>
-          {isPossibleToTakeForeplan && user?.config.FOREPLAN && (
-            <ForeplanCourseCheckbox
-              key="foreplanCourseCheckbox"
-              code={code}
-              credits={credits}
-              name={name}
-            />
-          )}
-        </AnimatePresence>
-      </SecondaryBlockOuter>
+      ></SecondaryBlockOuter>
     </OuterCourseBox>
   );
 }
