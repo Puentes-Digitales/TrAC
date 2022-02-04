872aced3c80f6447f9f914f3c10cde0551161ed1
jonefg0 <jothagoo@gmail.com>
2022-02-03 23:23:27 -0300

    download button is now shown when info is displayed

--- client/src/components/dashboard/SearchBar.tsx
+++ client/src/components/dashboard/SearchBar.tsx
@@ -136,6 +136,18 @@ export const SearchBar: FC<{
     }
   }, [chosenCurriculum, searchResult?.curriculums]);
 
+  const [showGroupedDownloadBotton, setshowGroupedDownloadBotton] = useState(
+    false
+  );
+
+  useEffect(() => {
+    if (searchResult?.admission_types.length) {
+      setshowGroupedDownloadBotton(true);
+    } else {
+      setshowGroupedDownloadBotton(false);
+    }
+  }, [searchResult, showGroupedDownloadBotton]);
+
   useEffect(() => {
     if (
       (chosenAdmissionType === undefined &&
@@ -270,9 +282,14 @@ export const SearchBar: FC<{
         {isDirector && user?.config?.SHOW_GROUPED_VIEW && <GrupedMode />}
 
         {user?.admin && <MockingMode />}
-        {(student_id || groupedActive) && user?.config?.SHOW_DOWNLOAD && (
-          <DownloadWord />
-        )}
+
+        {(student_id || (groupedActive && showGroupedDownloadBotton)) &&
+          user?.config?.SHOW_DOWNLOAD && <DownloadWord />}
+
+        {/*groupedActive &&
+          showGroupedDownloadBotton &&
+          user?.config?.SHOW_DOWNLOAD && <DownloadWord />*/}
+
         {isDirector && !groupedActive && user?.config?.SHOW_STUDENT_LIST && (
           <StudentList
             program_id={program?.value}
@@ -376,6 +393,7 @@ export const SearchBar: FC<{
     groupedActive,
     onSearch,
     HELP_ENABLED,
+    showGroupedDownloadBotton,
   ]);
 
   useEffect(() => {
