f3d6f955222844295d839cc654e739302100f7fd
Jonefg0 <jothagoo@gmail.com>
2022-10-17 13:49:04 -0300

    admin case login created

--- api/resolvers/auth/helpdesk.ts
+++ api/resolvers/auth/helpdesk.ts
@@ -10,9 +10,15 @@ export class HelpdeskResolver {
   async sendCredentials(
     @Arg("email") email: string,
     @Arg("Name") Name: string,
-    @Arg("LastName") LastName: string
+    @Arg("LastName") LastName: string,
+    @Arg("type") type: boolean
   ) {
-    const url = process.env.HELPDESK_LOGIN_URL || "";
+    var url = "";
+    if (type) {
+      url = process.env.HELPDESK_ADMIN_LOGIN_URL;
+    } else {
+      url = process.env.HELPDESK_CUSTOMER_LOGIN_URL;
+    }
     const customerCode = process.env.HELPDESK_CUSTOMER_CODE || "";
     console.log("se llama la funcion sendCredentials");
     verifyCustomer(email, Name, LastName);
